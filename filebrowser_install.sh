5723abfa0b0453ba8f6d6327c2e37dbb72865b7e
littlef0x <32670075+littlef0x@users.noreply.github.com>
2020-11-16 12:29:35 +0800

    Add port

--- filebrowser_install.sh
+++ filebrowser_install.sh
@@ -2,6 +2,7 @@
 
 # input display directory
 read -r -p 'Directory to display: ' displaydir
+read -r -p 'Port: ' port
 if [[ ! -d $displaydir ]]; then
 	sudo mkdir -p $displaydir
 	sudo chown ubuntu:ubuntu $displaydir
@@ -19,4 +20,5 @@ sudo chmod 775 /etc/filebrowser && sudo chown ubuntu:ubuntu /etc/filebrowser
 # start service
 sudo curl -s https://raw.githubusercontent.com/littlef0x/LittleScripts/master/filebrowser.service -o /etc/systemd/system/filebrowser.service
 sudo sed -i "s~\/data~$displaydir~" /etc/systemd/system/filebrowser.service
+sudo sed -i "s~8080~$port~" /etc/systemd/system/filebrowser.service
 sudo systemctl daemon-reload && sudo systemctl enable filebrowser && sudo systemctl start filebrowser
