b6b0dc476cf7e94e8d7548b09ac33647483cd810
littlef0x <32670075+littlef0x@users.noreply.github.com>
2020-12-06 15:24:06 +0800

    Add judgement for free space in /boot.

--- remove_unused_kernel.sh
+++ remove_unused_kernel.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Remove all kernels excluding newest one and the on in using
 
+used_disk=$(df -h | grep /boot | awk '{print $5}' | awk -F '%' '{print $1}')
+if [[ $used_disk -lt 80 ]]; then
+	echo 'Enough space for /boot.'
+	exit 1
+fi
+
 # List all kernels & exclude one in using, linux-image-generic, and newest one
 kernel_version=$(dpkg --list | grep linux-image | grep -v `uname -r` | grep -v linux-image-generic | sed '$d' | awk '{print $2}' | awk -F '-' '{print $3"-"$4}')
 # remove kernels in the list
